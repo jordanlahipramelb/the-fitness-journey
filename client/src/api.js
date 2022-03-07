acff4037a10dd91c14f5b56fdf786337eaa4794e
Jordan Lahip-Ramelb <jordan.lahipramelb@gmail.com>
2022-03-07 14:51:08 -0800

    commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL =
+  process.env.REACT_APP_BASE_URL || "https://thefitnessjourney.herokuapp.com/";
 
 /** API Class.
  *
