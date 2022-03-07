7861e5a65e77f8099f40959fae5c0a293499c019
Jordan Lahip-Ramelb <jordan.lahipramelb@gmail.com>
2022-03-07 13:57:46 -0800

    commit

--- app.js
+++ app.js
@@ -26,16 +26,14 @@ app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./client/public/index.html")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "./client/public/index.html", "index.html")
-  );
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.use("/auth", authRoutes);
