df3a69d12bee53c5ada9b7106f64992b342065ff
Jordan Lahip-Ramelb <jordan.lahipramelb@gmail.com>
2022-03-07 11:49:01 -0800

    2nd commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require("path");
 const cors = require("cors");
 const { NotFoundError } = require("./expressError");
 const { authenticateJWT } = require("./middleware/auth");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 const app = express();
 
 /** Routes */
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use(morgan("tiny"));
+app.use(morgan("tiny"));
 // app.use(authenticateJWT);
 
 app.get("/", (req, res) => {
@@ -54,4 +54,4 @@ app.use((err, req, res, next) => {
   });
 });
 
- module.exports = app;
+module.exports = app;
